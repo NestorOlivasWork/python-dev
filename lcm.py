54e776b8d4c90a67fccaa26dbbc30bf5da7f3fdd
johnny125999 <alexjohnny.business@gmail.com>
2023-11-05 17:15:25 -0800

    Least Common Multiple

---
+++ lcm.py
@@ -0,0 +1,6 @@
+import math
+print("Calculates LCM two numbers\n")
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+lcm = (a * b / math.gcd(a, b))
+print("Least Common Multiple: ", lcm)
